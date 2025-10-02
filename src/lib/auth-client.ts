a045cea6148aaf4475a5a26f49cdd9d44dd820d8
Felipe Marcos <felipe.ti404@gmail.com>
2025-10-02 00:58:24 -0300

    correção do lib

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,8 @@ import { createAuthClient } from "better-auth/react";
 import "@/types/auth"; // Importa as extensões de tipo
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL:
+    process.env.BETTER_AUTH_URL || "https://imobiliaria.admfelipemarcos.site",
 });
 
 export const { useSession } = authClient;
